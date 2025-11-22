a72746836594a477473ef27c97567fded63f63cb
tx0t1 <schwarzjoel.bautzen@gmail.com>
2025-11-22 18:57:36 +0100

    Add fxmanifest for Simple Door Lock Script

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DEIN_NAME'
+description 'Simple Door Lock Script'
+
+shared_script '@ox_lib/init.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
